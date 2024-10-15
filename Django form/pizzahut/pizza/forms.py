3265d7830da7327dfd7a4fe144cbaf992041a616
Nowfic hussain <nowfichussain@gmail.com>
2024-10-15 09:52:37 +0530

    Add files via upload

---
+++ Django form/pizzahut/pizza/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Pizza
+
+# class PizzaForm(forms.Form): #django form class
+#     topping1 = forms.CharField(label='topping1',max_length=100)
+#     topping2 = forms.CharField(label='topping2',max_length=100)
+#     size = forms.ChoiceField(label='Size',choices=[('Small','Small')
+#                         ,('Medium','Medium'),('Large','Large')])
+
+class PizzaForm(forms.ModelForm):
+    class Meta:
+        model = Pizza
+        fields =['topping1','topping2','size']
+
+class MultiplePizzaForm(forms.Form):
+    number = forms.IntegerField(min_value=2,max_value=10)
